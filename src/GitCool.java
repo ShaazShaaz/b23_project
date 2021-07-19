425ee1755f16e24459c5835ea409d1679dc6dd9d
Shazia <shaziasenol@gmail.com>
2021-07-19 16:42:36 -0400

    added a commit

--- src/GitCool.java
+++ src/GitCool.java
@@ -2,4 +2,7 @@ public class GitCool {
     // B23 is learning Git
     //making some sense now
     //B23 is the best23
+
+    // we have added our github account
+    // we make sure to click apply and ok
 }
